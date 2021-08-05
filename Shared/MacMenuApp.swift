e4a1f2ab869acddcf2cc9502431c7abd6d76ae67
David Amunga <david.onyimbo@gmail.com>
2021-08-05 11:33:29 +0300

    Initial Commit

---
+++ Shared/MacMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacMenuApp.swift
+//  Shared
+//
+//  Created by David Amunga on 05/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct MacMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
